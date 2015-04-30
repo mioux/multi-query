534487db88a78155078f47eca871a72b50fa1d84
Sylvain RUMEU <mioux-git@insertcoins.net>
2015-04-30 16:00:32 +0200

    Gestion de MySQL (en cours, non testé)

---
+++ MultiQuery/MySqlDataSourceEnumerator.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Crée par SharpDevelop.
+ * Utilisateur: SRUMEU
+ * Date: 30/04/2015
+ * Heure: 14:21
+ * 
+ */
+using System;
+using System.Data;
+
+namespace MultiQuery.MySqlDataSourceEnumerator
+{
+	/// <summary>
+	/// Description of MySqlDataSourceEnumerator.
+	/// </summary>
+	public class Instance
+	{
+		public static DataTable GetDataSources()
+		{
+			return new DataTable();
+		}
+	}
+}
